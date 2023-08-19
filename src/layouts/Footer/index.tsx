f25777cfc3523be53371a0ca82da2e365c3c949a
顶级压力小子 <45672942@qq.com>
2023-08-19 18:12:16 +0800

    feat(Footer): add Footer component

---
+++ src/layouts/Footer/index.tsx
@@ -0,0 +1,24 @@
+import './style.less'
+export default function Footer() {
+    return (
+        <footer className='footer'>
+            <div
+                data-message='If you are deploying to your own public website, please do not modify it unless you have permission from the original author.'
+            >
+                Made by
+                <a
+                    className='link'
+                    href='https://blog.gogogo7.com/'
+                    target='_blank'
+                    rel='nofollow noopener noreferrer'
+                >
+                    顶级压力小子
+                </a>
+            </div>
+
+            <div className='divider'>|</div>
+
+            {/*<div className='locale' @click='switchLocale'>*/}
+        </footer>
+    )
+}
