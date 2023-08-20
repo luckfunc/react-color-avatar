1b08b841de960c33162d39e0d3a1595495a9c0a4
顶级压力小子 <45672942@qq.com>
2023-08-20 17:01:20 +0800

    feat: Add ColorListItem components

---
+++ src/components/color-list-item/index.tsx
@@ -0,0 +1,41 @@
+import { SETTINGS } from '../../constants';
+import { AvatarOption } from '../../types';
+
+interface IProps {
+    avatarOption: AvatarOption
+    setAvatarOption: (newOption: AvatarOption) => void
+}
+
+export default function ColorListItem(props: IProps) {
+    const { setAvatarOption, avatarOption } = props;
+    const onSwitchBgColor = (bgColor: string) => {
+        if (bgColor !== avatarOption.background.color) {
+            setAvatarOption({
+                ...avatarOption,
+                background: { ...avatarOption.background, color: bgColor },
+            })
+        }
+    }
+    return (
+        <ul className='color-list'>
+            {
+                SETTINGS.backgroundColor.map((bgColor: string) => {
+                    return (
+                        <li
+                            key={bgColor}
+                            className={`color-list__item ${
+                                bgColor === avatarOption.background.color ? 'active' : ''
+                            } ${bgColor === 'transparent' ? 'transparent' : ''}`}
+                            onClick={() => onSwitchBgColor(bgColor)}
+                        >
+                            <div
+                                style={{ background: bgColor }}
+                                className='bg-color'
+                            />
+                        </li>
+                    )
+                })
+            }
+        </ul>
+    );
+}
