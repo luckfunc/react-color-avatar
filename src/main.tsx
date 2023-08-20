4d7903d5ad433b5f4034cfe9b407f5d31fde3f70
顶级压力小子 <45672942@qq.com>
2023-08-20 17:03:17 +0800

    feat: add store & import reset css & import global less

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import { Provider } from 'react-redux';
+import store from '../src/store/index';
+import './styles/reset.css';
+import '../src/styles/global.less';
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Provider store={store}>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>,
+    </Provider>
 )
