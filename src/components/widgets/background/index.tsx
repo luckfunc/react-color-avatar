77b9061293afd86887da7759ac3406282f832cec
顶级压力小子 <45672942@qq.com>
2023-08-20 16:59:20 +0800

    feat: add Background components

---
+++ src/components/widgets/background/index.tsx
@@ -0,0 +1,12 @@
+import { AvatarOption } from '../../../types';
+import './style.less';
+
+interface BackgroundProps {
+    color: AvatarOption['background']['color']
+}
+export default function Background(props: BackgroundProps) {
+    const { color } = props;
+    return (
+        <div className='avatar-background' style={{ background: color }}></div>
+    )
+}
