ec4b0fe70a624fecbb0080c9b63b9750bc2a824f
顶级压力小子 <45672942@qq.com>
2023-08-20 17:06:10 +0800

    feat: add action menu

---
+++ src/components/action-bar/index.tsx
@@ -0,0 +1,79 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { ActionType } from '../../enums';
+import IconBack from '../../assets/icons/icon-back.svg';
+import IconCode from '../../assets/icons/icon-code.svg';
+import IconFlip from '../../assets/icons/icon-flip.svg';
+import IconNext from '../../assets/icons/icon-next.svg';
+import { AppState } from '../../types';
+import './style.less'
+
+interface IProps {
+    flipped: boolean
+    setFlipped: (flipped: boolean) => void
+}
+
+export default function ActionBar(props: IProps) {
+    const { setFlipped, flipped } = props;
+    const history = useSelector((state: AppState) => state.history);
+    const canUndo = history.past.length > 0;
+    const canRedo = history.future.length > 0;
+    const dispatch = useDispatch();
+
+    const handleActionClick = (actionType: 'undo' | 'redo' | 'flip' | 'code') => {
+        switch (actionType) {
+            case ActionType.Undo:
+                dispatch({ type: actionType });
+                break;
+            case ActionType.Redo:
+                dispatch({ type: actionType });
+                break;
+            case ActionType.Flip:
+                setFlipped(!flipped);
+                break;
+            case ActionType.Code:
+                dispatch({ type: actionType });
+                break;
+            default:
+            // 默认处理
+        }
+    };
+
+    const actions = [
+        {
+            type: ActionType.Undo,
+            icon: IconBack,
+            tip: 'Undo',
+            disabled: !canUndo,
+        },
+        {
+            type: ActionType.Redo,
+            icon: IconNext,
+            tip: 'Redo',
+            disabled: !canRedo,
+        },
+        {
+            type: ActionType.Flip,
+            icon: IconFlip,
+            tip: 'Flip',
+        },
+        {
+            type: ActionType.Code,
+            icon: IconCode,
+            tip: 'Code',
+        },
+    ];
+    return (
+        <div className='action-menu'>
+            {actions.map((ac) => (
+                <div
+                    key={ac.type}
+                    className={`menu-item ${ac.disabled ? 'disabled' : ''}`}
+                    title={ac.tip}
+                    onClick={() => handleActionClick(ac.type)}
+                >
+                    <img src={ac.icon} alt={ac.tip} />
+                </div>
+            ))}
+        </div>
+    )
+}
