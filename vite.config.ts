acdda2602105bf5f5c0d03f9d6ed7e671b5cd86f
xdd <45672942@qq.com>
2024-08-11 22:55:23 +0800

    reflector: Optimize alias

--- vite.config.ts
+++ vite.config.ts
@@ -8,12 +8,24 @@ export default defineConfig({
     alias: {
       '@': '/src',
       '@assets': '/src/assets',
+      '@assets/*': '/src/assets/*',
+      'assets/*': '/src/assets/*',
       '@utils': '/src/utils',
+      '@utils/*': '/src/utils/*',
       '@hooks': '/src/hooks',
+      '@hooks/*': '/src/hooks/*',
       '@components': '/src/components',
+      '@components/*': '/src/components/*',
       '@constants': '/src/constants',
+      '@constants/*': '/src/constants/*',
       '@types': '/src/types',
+      '@types/*': '/src/types/*',
       '@layouts': '/src/layouts',
+      '@layouts/*': '/src/layouts/*',
+      '@store': '/src/store',
+      '@store/*': '/src/store/*',
+      '@enums': '/src/enums',
+      '@enums/*': '/src/enums/*',
     }
   }
 })
